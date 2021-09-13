a06e05e1e72dcb0ce24373e32e07d885fc1f4adb
MaddHattPatt <MaddHatt.pm@gmail.com>
2021-09-13 13:33:28 -0400

    Basic file scanning

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
 public class Main {
+    public static final String PATH_MIX_DOWNS = "Get later - mix downs";
+    public static final String PATH_STEMS = "Get later - stems";
+    public static final String PATH_TEMP = "C:/Users/Patri/Desktop/Code Projects/MaddHattPatt-BlenderTools"; 
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Path dir = Paths.get(PATH_TEMP);
+        Files.walk(dir).forEach(path -> showFile(path.toFile()));
+    }
+
+    public static void showFile(File file) {
+        if (file.isDirectory()) {
+            System.out.println(("Directory: " + file.getAbsolutePath()));
+        } else {
+            System.out.println(("File: " + file.getAbsolutePath()));
+        }
     }
 }
