a2d0b5473066cce5e2cbdfd4f233a1d9b267895e
Patt <MaddHatt.pm@gmail.com>
2021-09-13 16:19:17 -0400

    removed song object testing

--- src/Main.java
+++ src/Main.java
@@ -11,22 +11,8 @@ public class Main {
     public static final String PATH_TEMP = "C:/Users/Patri/Desktop/Code Projects/MaddHattPatt-BlenderTools";
 
     public static void main(String[] args) throws Exception {
-        Song test = new Song();
-        ArrayList<Song> exampleList = new ArrayList<>();
-        exampleList.add(new Song());
-        exampleList.add(new Song());
-        exampleList.add(new Song());
-
-        exampleList.get(0).songName = "This is a different name";
-        for (int id = 0; id < exampleList.size(); id++) {
-            export += exampleList.get(id).toExportHashTable();
-            export += "\n";
-        }
-
-        System.out.println(export);
-
-        // Path dir = Paths.get(PATH_TEMP);
-        // Files.walk(dir).forEach(path -> showFile(path.toFile()));
+        Path dir = Paths.get(PATH_TEMP);
+        Files.walk(dir).forEach(path -> showFile(path.toFile()));
     }
 
     public static void showFile(File file) {
